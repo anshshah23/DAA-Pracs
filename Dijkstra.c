ec7f541e58562df0cd839ac3a5c129c248132aa6
anshshah23 <anshzeal2328@gmail.com>
2024-05-04 14:46:44 +0530

    c

--- Dijkstra.c
+++ Dijkstra.c
@@ -1,6 +1,4 @@
-// C program for Dijkstra's single source shortest path
-// algorithm. The program is for adjacency matrix
-// representation of the graph
+// C program for Dijkstra's single source shortest path algorithm. The program is for adjacency matrix representation of the graph
 
 #include <limits.h>
 #include <stdbool.h>
@@ -9,9 +7,7 @@
 // Number of vertices in the graph
 #define V 9
 
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
+// A utility function to find the vertex with minimum distance value, from the set of vertices not yet included in shortest path tree
 int minDistance(int dist[], bool sptSet[])
 {
 	// Initialize min value
@@ -24,8 +20,7 @@ int minDistance(int dist[], bool sptSet[])
 	return min_index;
 }
 
-// A utility function to print the constructed distance
-// array
+// A utility function to print the constructed distance array
 void printSolution(int dist[])
 {
 	printf("Vertex \t\t Distance from Source\n");
@@ -33,22 +28,13 @@ void printSolution(int dist[])
 		printf("%d \t\t\t\t %d\n", i, dist[i]);
 }
 
-// Function that implements Dijkstra's single source
-// shortest path algorithm for a graph represented using
-// adjacency matrix representation
+// Function that implements Dijkstra's single source shortest path algorithm for a graph represented using adjacency matrix representation
 void dijkstra(int graph[V][V], int src)
 {
-	int dist[V]; // The output array. dist[i] will hold the
-				// shortest
-	// distance from src to i
+	int dist[V]; // The output array. dist[i] will hold the shortest distance from src to i
+	bool sptSet[V]; //sptSet[i] will be true if vertex i is included in shortest path tree or shortest distance from src to i is finalized
 
-	bool sptSet[V]; // sptSet[i] will be true if vertex i is
-					// included in shortest
-	// path tree or shortest distance from src to i is
-	// finalized
-
-	// Initialize all distances as INFINITE and stpSet[] as
-	// false
+	// Initialize all distances as INFINITE and stpSet[] as false
 	for (int i = 0; i < V; i++)
 		dist[i] = INT_MAX, sptSet[i] = false;
 
@@ -57,22 +43,15 @@ void dijkstra(int graph[V][V], int src)
 
 	// Find shortest path for all vertices
 	for (int count = 0; count < V - 1; count++) {
-		// Pick the minimum distance vertex from the set of
-		// vertices not yet processed. u is always equal to
-		// src in the first iteration.
+		// Pick the minimum distance vertex from the set of vertices not yet processed. u is always equal to src in the first iteration.
 		int u = minDistance(dist, sptSet);
 
-		// Mark the picked vertex as processed
-		sptSet[u] = true;
+		sptSet[u] = true; // Mark the picked vertex as processed
 
-		// Update dist value of the adjacent vertices of the
-		// picked vertex.
+		// Update dist value of the adjacent vertices of the picked vertex.
 		for (int v = 0; v < V; v++)
 
-			// Update dist[v] only if is not in sptSet,
-			// there is an edge from u to v, and total
-			// weight of path from src to v through u is
-			// smaller than current value of dist[v]
+			// Update dist[v] only if is not in sptSet, there is an edge from u to v, and total weight of path from src to v through u is smaller than current value of dist[v]
 			if (!sptSet[v] && graph[u][v]
 				&& dist[u] != INT_MAX
 				&& dist[u] + graph[u][v] < dist[v])
