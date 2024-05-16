fe98c5fee039457aa3bd365944706049b00d1a48
Ansh Shah <122973942+anshshah23@users.noreply.github.com>
2024-05-16 14:59:24 +0530

    Update BinarySearch.c

--- Analysis of Basic Algos/BinarySearch.c
+++ Analysis of Basic Algos/BinarySearch.c
@@ -7,7 +7,7 @@
 
 int bs(int a[], int l, int r, int x) {
     while (l <= r) {
-        int m = l + (r - l) / 2; // Calculate the middle index of the array
+        int m = (l + r) / 2; // Calculate the middle index of the array
         if (a[m] == x) return m; // If the middle element is equal to x, return the index
         if (a[m] < x) l = m + 1; // If the middle element is less than x, search in the right half of the array
         else r = m - 1; // If the middle element is greater than x, search in the left half of the array
